23192254061cd1b5e83f7c1fb7b97bea8af4a291
Rick Kosbab <rjk0031@auburn.edu>
2025-04-15 16:00:26 -0500

    working implementation using async,

--- BiAmpIPInterface.py
+++ BiAmpIPInterface.py
@@ -1,66 +1,85 @@
 from mojo import context
-import socket
 import asyncio
+transport = None
+class BiampTelnetClientProtocol(asyncio.Protocol):
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('10.184.42.54', 23))
-def handleData(data):
-    context.log.info(str(data))
-    wontCommands = []
-    dontCommands = []
-    i = 0
-
-    while i < len(data):
-        if data[i:i+2] == bytes([255,253]):
-            wontCommands.append(data[i+2])
-        elif data[i:i+2] == bytes([255,251]):
-            dontCommands.append(data[i+2])
-        i += 3
-
-    context.log.info("Wont Command Count: " + str(len(wontCommands)))
-    context.log.info("Dont Command Count: " + str(len(dontCommands)))
-
-    for byte in wontCommands:
-        response = bytes([255, 252, byte])
-        clientsocket.send(response)
-    wontCommands.clear()
-
-    for byte in dontCommands:
-        response = bytes([255, 254, byte])
-        clientsocket.send(response)
+    def __init__(self, on_con_lost):
+        self.on_con_lost = on_con_lost
+        self.transport = None
+        self.loggedIn = False
     
-    wontCommands.clear()
-    dontCommands.clear()
-handleData(clientsocket.recv(2048))
-handleData(clientsocket.recv(2048))
-handleData(clientsocket.recv(2048))
-data = clientsocket.recv(2048)
-context.log.info(str(data))
-clientsocket.send("admin".encode('ascii') + bytes([10]))
-handleData(clientsocket.recv(2048))
-handleData(clientsocket.recv(2048))
-data = clientsocket.recv(2048)
-context.log.info(str(data))
-clientsocket.send("#E!ki986".encode('ascii') + bytes([10]))
-data = clientsocket.recv(2048)
-context.log.info(str(data))
-data = clientsocket.recv(2048)
-context.log.info(str(data))
+    def connection_made(self, transport):
+        self.transport = transport
+        transport = transport
+        context.log.info("Connection Established!")
+    
+    def data_received(self, data):
+        # Per Biamp Telnet Session Negotiation, if first byte is 0xFF (255) its a Interpret as Command
+        # Must respond to any command as listed in link below
+        # https://support.biamp.com/Tesira/Control/Telnet_session_negotiation_in_Tesira
+        if data[0] == 255:
+            context.log.info("COMMAND RECEIVED")
+            wontCommands = []
+            dontCommands = []
+            i = 0
+            
+            # Commands come as 3 bytes starting with 255 and then will be a 253 or 251
+            while i < len(data):
+                if data[i:i+2] == bytes([255,253]):
+                    wontCommands.append(data[i+2])
+                elif data[i:i+2] == bytes([255,251]):
+                    dontCommands.append(data[i+2])
+                i += 3
 
-clientsocket.send("2108WlsLvl set level 1 -60".encode('ascii') + bytes([10]))
-data = clientsocket.recv(2048)
-context.log.info(str(data))
+            # respond to any IAC messages
+            for byte in wontCommands:
+                response = bytes([255, 252, byte])
+                self.transport.write(response)
+            wontCommands.clear()
 
-clientsocket.send("2108WlsLvl set level 1 0".encode('ascii') + bytes([10]))
-data = clientsocket.recv(2048)
-context.log.info(str(data))
+            for byte in dontCommands:
+                response = bytes([255, 254, byte])
+                self.transport.write(response)
+            dontCommands.clear()
+        
+        # If we see login or password, attempt to log in
+        elif "login" in str(data):
+            context.log.info("Saw login Prompt")
+            self.transport.write("admin".encode('ascii') + bytes([10]))
 
-clientsocket.send("2108WlsLvl set level 1 -60".encode('ascii') + bytes([10]))
-data = clientsocket.recv(2048)
-context.log.info(str(data))
-clientsocket.send("2108WlsLvl set level 1 -40".encode('ascii') + bytes([10]))
-data = clientsocket.recv(2048)
-context.log.info(str(data))
+        elif "Password" in str(data):
+            context.log.info("Saw password Prompt")
+            self.transport.write("#E!ki986".encode('ascii') + bytes([10]))
+        elif "Welcome" in str(data):
+            context.log.info ("Login Successful")
+            self.loggedIn = True
+        else:
+            context.log.info(str(data))
+
+    
+    def connection_lost(self, exc):
+        context.log.info('The server closed the connection')
+        self.on_con_lost.set_result(True)
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    on_con_lost = loop.create_future()
+
+    transport, protocol = await loop.create_connection(lambda: BiampTelnetClientProtocol(on_con_lost),'10.184.42.54', 23)
+
+    try:
+        await on_con_lost
+    finally:
+        transport.close()
+
+def call(path, args):
+    contxt.log.info(path)
+    context.log.info(args)
+
+asyncio.run(main())
+
+#clientsocket.send("2108WlsLvl set level 1 -40".encode('ascii') + bytes([10]))
 
 # leave this as the last line in the Python script
 context.run(globals())
